fac18f37817f5755ff293942f2e826d0fea5f9d7
Brad Gessler <bradgessler@gmail.com>
2022-06-06 12:31:34 -0700

    Remove bundle

--- lib/generators/monolith/install/install_generator.rb
+++ lib/generators/monolith/install/install_generator.rb
@@ -87,7 +87,6 @@ class Monolith::InstallGenerator < Rails::Generators::Base
   def post_install_message
     say_status "next steps", <<~DOCS
     [ ] Run `rails db:migrate`
-    [ ] Run `bundle install`
     [ ] Launch the server via `dev`
     [ ] Go to `http://localhost:3000`
     DOCS
